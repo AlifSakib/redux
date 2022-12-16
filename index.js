772e28868204c36121c2064d4b75bfa04d5db6d4
AlifSakib <alifsakib@gmail.com>
2022-12-16 12:38:17 +0600

    added redux store and dispatch action

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 function orderCake() {
-  {
-    type: CAKE_ORDERED;
-    quantity: 1;
-  }
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
 }
 
 // (previousState, action) => newState
@@ -25,3 +28,16 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("UpdatedStore", store.getState())
+);
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
+store.dispatch(orderCake());
