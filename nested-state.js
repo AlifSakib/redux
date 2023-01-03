e19393ebeb6d312b47b2aeeda02a66328e59d75c
AlifSakib <alifsakib@gmail.com>
2023-01-03 23:02:08 +0600

    added updated user address

--- nested-state.js
+++ nested-state.js
@@ -1,3 +1,4 @@
+const redux = require("redux");
 const initialState = {
   name: "alif",
   address: {
@@ -6,3 +7,35 @@ const initialState = {
     state: "MA",
   },
 };
+
+const STREET_UPDATED = "STREET_UPDATED";
+const updateStreet = (street) => {
+  return {
+    type: STREET_UPDATED,
+    payload: street,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "STREET_UPDATED":
+      return {
+        ...state,
+        address: {
+          ...state.address,
+          street: action.payload,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
+
+const store = redux.createStore(reducer);
+console.log("Initial State", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State", store.getState());
+});
+store.dispatch(updateStreet("456 Main st"));
+unsubscribe();
